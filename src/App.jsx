ba802e62eea144ba92e33af5ec22f0377b184678
Johan Castillo <jcjohan2707@gmail.com>
2021-01-05 15:26:02 -0400

    TopBar Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // ***** Components *****
 import Menu from './components/menu/Menu';
+import TopBar from './components/top-bar/TopBar';
 
 
 function App() {
   return (
     <>
+      <TopBar />
       <Menu />
     </>
   );
